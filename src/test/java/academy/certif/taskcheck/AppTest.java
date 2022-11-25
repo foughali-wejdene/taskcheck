e0b5ced07978353a9bb89e58951c1a6074ce3d7b
foughali-wejdene <wejden.foughali@outlook.fr>
2022-11-25 15:23:07 +0100

    second

--- src/test/java/academy/certif/taskcheck/AppTest.java
+++ src/test/java/academy/certif/taskcheck/AppTest.java
@@ -1,14 +1,21 @@
 package academy.certif.taskcheck;
 
-import static org.junit.Assert.assertTrue;
-
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
+    @Test
+    public final void EntityCheckable() {
+        fail("Not yet implemented"); // TODO
+    }
+
     /**
      * Rigorous Test :-)
      */
